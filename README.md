b27e1505bee0628e23fdef962efb3740a285f6f1
rom <47617159+leedokchidok19@users.noreply.github.com>
2021-12-15 23:14:43 +0900

    서비스키 commit 취소 후 commit

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
-# clone-vanillaJs-todoList
+# Project
+### clone-vanillaJs-todoList
+- 바닐라스크립트로 구현한 todoList  
+강의를 바탕으로 간단한 기능 추가   
+requirements 참고  
+---
+# Demo
+[clone-vanillaJs-todoList 사이트 이동](https://leedokchidok19.github.io/clone-vanillaJs-todoList/)
 ---
 ### Design
 ##### Preview
-<img src="images/preview.png" width="50%" height="auto" title="todoList" alt="preview"></img>
+<p align="center">
+    <img src="images/preview.png" width="50%" height="auto" title="todoList" alt="preview"></img>
+</p>
+   
 ##### requirements
 + Single Page Application(SPA)
 + Clock
 + Weather API
 + Todo
++ localstorage
 ---
 ### Reference
-[nomadcoders](https://nomadcoders.co/)
++ [nomadcoders](https://nomadcoders.co/)
 
 ---
-### API
-[OpenWeatherMap](https://openweathermap.org/)
+# API
++ [OpenWeatherMap](https://openweathermap.org/)
++ [공공데이터포털](https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15084084)
 ---
---
+++ js/weather-api.js
@@ -0,0 +1,25 @@
+/*
+공공데이터포털
+*/
+
+var xhr = new XMLHttpRequest();
+var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst';   /*URL*/
+
+var queryParams =   '?' +   encodeURIComponent('serviceKey')    +   '=' +   '서비스키';  /*Service Key - 서비스키*/
+queryParams     +=  '&' +   encodeURIComponent('pageNo')        +   '=' +   encodeURIComponent('1');        /*페이지 번호*/
+queryParams     +=  '&' +   encodeURIComponent('numOfRows')     +   '=' +   encodeURIComponent('1000');     /*한 페이지 결과 수*/
+queryParams     +=  '&' +   encodeURIComponent('dataType')      +   '=' +   encodeURIComponent('XML');      /*응답자료형식*/
+queryParams     +=  '&' +   encodeURIComponent('base_date')     +   '=' +   encodeURIComponent('20210628'); /*발표일자*/
+queryParams     +=  '&' +   encodeURIComponent('base_time')     +   '=' +   encodeURIComponent('0600');     /*발표시각*/
+queryParams     +=  '&' +   encodeURIComponent('nx')            +   '=' +   encodeURIComponent('55');       /*예보지점 X 좌표*/
+queryParams     +=  '&' +   encodeURIComponent('ny')            +   '=' +   encodeURIComponent('127');      /*예보지점 Y 좌표*/
+
+xhr.open('GET', url + queryParams);
+
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+        alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+    }
+};
+
+xhr.send('');
