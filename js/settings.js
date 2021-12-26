884ce97db1e521eef3fcf8293e61bf5d34b36fa6
rom <47617159+leedokchidok19@users.noreply.github.com>
2021-12-26 20:31:36 +0900

    add set bar

--- js/settings.js
+++ js/settings.js
@@ -2,4 +2,7 @@ const cog = document.querySelector('#cog');
 
 cog.addEventListener('click', function(){
 
+    setBarList = document.querySelectorAll('#set__bar li');
+
+    setBarList.forEach(element => element.classList.remove('d-none'));
 });
